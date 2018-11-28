963c5ea4433ed06bf29e4e0f58b02f69e4036bbf
Ben Chapman <bchapman@pivotal.io>
2018-11-28 15:41:11 +0000

    add importer

--- importer/main.go
+++ importer/main.go
@@ -5,8 +5,6 @@ import (
 	"fmt"
 	"os"
 	"strconv"
-	"strings"
-	"time"
 
 	"code.benchapman.ie/dublinbikeparking/apiv0"
 	"github.com/jinzhu/gorm"
@@ -30,34 +28,45 @@ func main() {
 	}
 	db.LogMode(true)
 
-	r, _ := os.Open(os.Args[1])
+	r, _ := os.Open("import.csv")
 	records, _ := csv.NewReader(r).ReadAll()
-	for _, record := range records {
-		var stands []apiv0.Stand
+	for ln, record := range records {
+		var stand apiv0.Stand
 
-		db.Where("lat LIKE ? AND lng LIKE ?", record[1][:8]+"%", record[0][:8]+"%").Find(&stands)
+		fmt.Printf("%d - %#v", ln, record)
+		lat := record[27][1:9]
+		lng := record[28][:8]
+		db.Where("lat LIKE ? AND lng LIKE ?", lat+"%", lng+"%").First(&stand)
 
-		noStands, err := strconv.Atoi(strings.TrimSuffix(record[7], ".0"))
-		if err != nil {
-			fmt.Printf("%s\n", err)
-		}
+		stands, _ := strconv.Atoi(record[6])
+		db.Model(&stand).Where("lat LIKE ? AND lng LIKE ?", lat+"%", lng+"%").Updates(map[string]interface{}{
+			"name":             record[5],
+			"number_of_stands": stands,
+			"type":             record[0],
+			"source_id":        fmt.Sprintf("%d", ln),
+		})
 
-		if len(stands) == 0 {
-			lat, _ := strconv.ParseFloat(record[1], 64)
-			lng, _ := strconv.ParseFloat(record[0], 64)
-
-			db.Create(&apiv0.Stand{
-				Model: gorm.Model{
-					CreatedAt: time.Now(),
-					UpdatedAt: time.Now(),
-				},
-				Lat:            lat,
-				Lng:            lng,
-				Name:           record[2],
-				SourceID:       record[4],
-				NumberOfStands: noStands,
-				Type:           record[8],
-			})
-		}
+		//		noStands, err := strconv.Atoi(strings.TrimSuffix(record[7], ".0"))
+		//		if err != nil {
+		//			fmt.Printf("%s\n", err)
+		//		}
+		//
+		//		if len(stands) == 0 {
+		//			lat, _ := strconv.ParseFloat(record[1], 64)
+		//			lng, _ := strconv.ParseFloat(record[0], 64)
+		//
+		//			db.Create(&apiv0.Stand{
+		//				Model: gorm.Model{
+		//					CreatedAt: time.Now(),
+		//					UpdatedAt: time.Now(),
+		//				},
+		//				Lat:            lat,
+		//				Lng:            lng,
+		//				Name:           record[2],
+		//				SourceID:       record[4],
+		//				NumberOfStands: noStands,
+		//				Type:           record[8],
+		//			})
+		//		}
 	}
 }
