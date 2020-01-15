1b7011823d8390f7e2160b2da12a49b7db1e0158
Katie Chapman <hello@katiechapman.ie>
2020-01-15 17:14:19 +0000

    Enable DublinBikes control on UI V2

--- static-vue/src/components/Map.vue
+++ static-vue/src/components/Map.vue
@@ -1,20 +1,35 @@
 <template>
   <l-map class='map' :zoom='zoom' :center='center' :options='mapOptions'>
-    <l-tile-layer :url='url'></l-tile-layer>
-    <l-marker-cluster :options='clusterOptions'>
+    <l-tile-layer :url='url' :options="tileLayerOptions"></l-tile-layer>
+    <l-marker-cluster ref="clusterRef" :options='clusterOptions'>
       <l-geo-json
         v-for='geojson in geojsons'
-        :key='geojson.type'
+        :key='geojson.features[0].properties.id'
         :geojson='geojson'
         :options='geojsonOptions'>
       </l-geo-json>
     </l-marker-cluster>
+    <l-control>
+      <div class="btn-group leaflet-selector">
+        <button v-for="bikeType in Object.keys(bikeTypes)" v-bind:key="bikeType"
+                class="btn btn-sm"
+                :class="{
+                  'btn-dark': bikeTypeIsActive(bikeType),
+                  'btn-outline-dark': !bikeTypeIsActive(bikeType) }"
+                @click="changeBikeType(bikeType)"
+        >
+          {{bikeType}}
+        </button>
+      </div>
+    </l-control>
   </l-map>
 </template>
 
 <script>
 import L from 'leaflet';
-import { LMap, LTileLayer, LGeoJson } from 'vue2-leaflet';
+import {
+  LMap, LTileLayer, LGeoJson, LControl,
+} from 'vue2-leaflet';
 import LMarkerCluster from 'vue2-leaflet-markercluster';
 import axios from 'axios';
 
@@ -32,6 +47,15 @@ const standIcons = {
   'Stainless Steel Curved': new StandIcon({ iconUrl: 'icons/stand_icon_ssc.png' }),
   Railing: new StandIcon({ iconUrl: 'icons/stand_icon_railing.png' }),
   DublinBikes: new StandIcon({ iconUrl: 'icons/station_icon_db.png' }),
+};
+
+const bikeTypes = {
+  'My Bike': {
+    query: 'dublinbikes=off',
+  },
+  DublinBikes: {
+    query: 'dublinbikes=only',
+  },
 };
 
 function standType(f, l) {
@@ -65,6 +89,7 @@ export default {
     'l-tile-layer': LTileLayer,
     'l-geo-json': LGeoJson,
     'l-marker-cluster': LMarkerCluster,
+    'l-control': LControl,
   },
   data() {
     return {
@@ -75,19 +100,36 @@ export default {
         maxClusterRadius: 50,
         disableClusteringAtZoom: 18,
       },
-      mapOptions: {
+      mapOptions: {},
+      tileLayerOptions: {
         maxZoom: 18,
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Stand Data: <a href="https://bleeperbike.com/">Bleeper Bikes</a>, <a href="https://data.smartdublin.ie/dataset/dcc_public_cycle_parking_stands">DCC</a> DublinBikes: <a href="https://developer.jcdecaux.com/#/opendata/vls?page=getstarted&contract=Dublin">JCDecaux</a>  | <a href="/privacy">Privacy</a>',
       },
       geojsonOptions: {
         onEachFeature: standType,
       },
+      bikeTypes,
+      activeBikeType: 'My Bike',
       geojsons,
     };
   },
+  methods: {
+    bikeTypeIsActive(bikeType) {
+      return bikeType === this.activeBikeType;
+    },
+    changeBikeType(bikeType) {
+      this.activeBikeType = bikeType;
+      this.geojsons.pop();
+      this.updateMap();
+    },
+    updateMap() {
+      axios.get(`/api/v0/stand?${this.bikeTypes[this.activeBikeType].query}`).then((response) => {
+        this.geojsons.push(response.data);
+      });
+    },
+  },
   created() {
-    axios.get('/api/v0/stand?dublinbikes=off').then((response) => {
-      this.geojsons.push(response.data);
-    });
+    this.updateMap();
   },
 };
 </script>
